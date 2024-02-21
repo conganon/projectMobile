bf8656b90aeb6163707d212bcb4fca1a00ec8c2d
conganon <minhtngcs190905@fpt.edu.vn>
2024-02-21 07:38:30 +0700

    upload

---
+++ contains/theme.js
@@ -0,0 +1,27 @@
+const COLORS = {
+    background  : '#292F3F',
+    white       : '#FFFFFF',
+    black       : '#000000',
+    title       : '#5C5C5B',
+    register    : '#FFDB5E',
+    main        : '#FFCB13',
+    red         : '#DD2E44',
+    room        : '#F1EDED',
+    grey        : 'grey',
+    green       : 'green',
+    loginAndlogout : '#fabf4a',
+    colorsThongtin: '#5A5858',
+    button : '#5a5858',
+}
+
+const FONTSIZE = {
+    welcome : 50,
+    infoName : 34,
+    h1 : 36,
+    h2 : 16,
+    h3 : 14,
+}
+
+export {
+    COLORS , FONTSIZE
+}
